639c7d351cf62786f5c02f42e2ab8f0026d8b041
swanmatch <swan_match@yahoo.co.jp>
2016-11-23 19:27:02 +0900

    init fin

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161123095207) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title",        limit: 255
+    t.string   "composer",     limit: 255
+    t.integer  "tempo",        limit: 4
+    t.integer  "key",          limit: 4
+    t.integer  "scale",        limit: 4
+    t.text     "score",        limit: 65535
+    t.integer  "lock_version", limit: 4,     default: 0, null: false
+    t.integer  "created_by",   limit: 4
+    t.integer  "updated_by",   limit: 4
+    t.integer  "deleted_by",   limit: 4
+    t.datetime "deleted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
